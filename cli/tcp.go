f68bd64b595d6ab6364a210e7aaa9df2a30f2395
dpwgc <281148314@qq.com>
2024-07-17 15:53:27 +0800

    connect优化

--- cli/tcp.go
+++ cli/tcp.go
@@ -8,16 +8,10 @@ import (
 )
 
 func TCP(remoteAddr string, localPort int, tunnelNum int) {
-	controlChan := make(chan bool, tunnelNum)
-	for i := 0; i < tunnelNum; i++ {
-		controlChan <- true
-	}
 	for {
-		_ = <-controlChan
 		// 连接到服务端
 		remoteConn, err := net.Dial("tcp", remoteAddr)
 		if err != nil {
-			controlChan <- true
 			base.Println(31, 40, fmt.Sprintf("remote server [%v] connection failed: %s", remoteAddr, err.Error()))
 			base.Logger.Error(fmt.Sprintf("error connecting to server: %v", err))
 			time.Sleep(1 * time.Second)
@@ -26,17 +20,12 @@ func TCP(remoteAddr string, localPort int, tunnelNum int) {
 
 		base.Println(32, 40, fmt.Sprintf("remote server [%v] connection success", remoteAddr))
 
-		go func() {
-
-			defer func() {
-				base.Println(33, 40, fmt.Sprintf("[%v] -> [%v] tunnel close", localPort, remoteAddr))
-				remoteConn.Close()
-				controlChan <- true
-			}()
+		func() {
 
 			var header = make([]byte, 8)
 			_, err = remoteConn.Read(header)
 			if err != nil {
+				remoteConn.Close()
 				base.Println(33, 40, fmt.Sprintf("remote server [%v] connection close: %s", remoteAddr, err.Error()))
 				base.Logger.Error(fmt.Sprintf("error reading remote connection: %v", err))
 				return
@@ -44,10 +33,9 @@ func TCP(remoteAddr string, localPort int, tunnelNum int) {
 
 			cmd := base.BytesToInt64(header)
 			if cmd == 1 {
-
 				localConn, err := net.Dial("tcp", fmt.Sprintf(":%v", localPort))
 				if err != nil {
-					localConn.Close()
+					remoteConn.Close()
 					base.Println(31, 40, fmt.Sprintf("local server [%v] connection failed: %s", localPort, err.Error()))
 					base.Logger.Error(fmt.Sprintf("error connecting to server: %v", err))
 					time.Sleep(1 * time.Second)
@@ -55,7 +43,13 @@ func TCP(remoteAddr string, localPort int, tunnelNum int) {
 				}
 				base.Println(32, 40, fmt.Sprintf("local server [%v] connection success", localPort))
 
-				base.CopyConn(localConn, remoteConn)
+				go base.CopyConn(localConn, remoteConn)
+
+			} else {
+				remoteConn.Close()
+				base.Println(31, 40, fmt.Sprintf("remote server [%v] command failed: %s", localPort, err.Error()))
+				base.Logger.Error(fmt.Sprintf("error command from server: %v", err))
+				time.Sleep(1 * time.Second)
 			}
 		}()
 	}
